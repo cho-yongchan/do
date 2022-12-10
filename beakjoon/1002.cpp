f30ff7bfa005dbd1330b57def9d6082f56370965
yongchan.cho <yongchan.cho@lge.com>
2022-12-10 14:46:19 +0900

    BOJ1002

--- beakjoon/1002.cpp
+++ beakjoon/1002.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int func(void){
-    
-    
+
     int x1,y1,r1,x2,y2,r2,ans;
-    cin >> x1 >> y2 >> r1 >> x2 >> y2 >> r2;
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
     
+    long int range = (r1+r2)*(r1+r2);
+    long int r = (r1-r2)*(r1-r2);
+
     long int tmp = ((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2));
-    int range2 = (r1+r2)*(r1+r2);
-    int range1 = (r1-r2)*(r1-r2);
-    
-    //cout << tmp << " " << range1 << " " << range2 << "\n";
-    if(tmp==range1){
+
+    if(tmp == 0 && (r1 == r2)){
         ans = -1;
     }
-    else if(tmp > range1 & tmp < range2){
-        ans = 2;
-    }
-    else if(tmp==range2){
+
+    else if((tmp==range)||(tmp==r)){
         ans = 1;
     }
-    else{
+    else if((tmp > range) || (tmp < r) || (tmp == 0 && r1 != r2)){
         ans = 0;
     }
-    
+    else{
+        ans = 2;
+    }
+
     return ans;
-    
+
 }
 int main(void){
-    
+
     int cnt;
     cin >> cnt;
-    
+
     for(int i = 0; i< cnt; i++){
         cout << func() << "\n";
     }
     return 0;
 }
-
